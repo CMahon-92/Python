76a2c3391bb6ec404ba2219ad9811dd7d25ea367
Carleena S. Mahon <71473354+CMahon-92@users.noreply.github.com>
2024-01-22 20:29:26 -0600

    Add files via upload

---
+++ objectAndDataStructureBasics.py
@@ -0,0 +1,36 @@
+#variables to use for equations
+x = 4
+y = 3
+
+#Addition
+addition = x + y
+print(addition)
+
+#subtraction
+subtract = x-y
+print(subtract)
+
+#multiplication
+multiply = x * y
+print(multiply)
+
+#division
+divide = x/y
+print(divide)
+
+#modulo
+modulo = x%y
+print(modulo)
+
+#exponent
+power = x**y
+print(power)
+
+#order of operation part 1
+part1 = x + y * y + x
+print(part1)
+
+#order of operation part 2
+part2 = (x+y) * (y+x)
+print(part2)
+
